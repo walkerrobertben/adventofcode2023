bec1d4212f62bb2569359186bd80956d8ecd812e
Ben Walker <63571454+walkerrobertben@users.noreply.github.com>
2023-12-03 18:56:54 +0000

    Solution 03 part 2

--- solutions/03/03.cpp
+++ solutions/03/03.cpp
@@ -1,20 +1,35 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <map>
+
+struct XY {
+	int x, y;
+
+	XY (int _x, int _y) {
+		x = _x;
+		y = _y;
+	}
+
+	bool operator<(const XY& other) const {
+        if (x == other.x) {
+            return y < other.y;
+        }
+        return x < other.x;
+    }
+};
 
 static bool isSymbol(char c) {
 	return !std::isdigit(c) && c != '.';
 }
 
-static void part1() {
+static std::vector<std::string> getEngine(int &width, int &height) {
 	std::ifstream file("solutions/03/data");
 
 	if (!file.is_open()) {
 		throw std::runtime_error("Failed to open file");
 	}
 
-	int width = 0;
-	int height = 0;
 	std::string line;
 	std::vector<std::string> lines;
 	while (std::getline(file, line)) {
@@ -22,6 +37,13 @@ static void part1() {
 		height += 1;
 		lines.push_back(line);
 	}
+
+	return lines;
+}
+
+static void part1() {
+	int width, height = 0;
+	std::vector<std::string> engine = getEngine(width, height);
 
 	int total = 0;
 
@@ -31,7 +53,7 @@ static void part1() {
 	for (int y = 0; y < height; y++) {
 		for (int x = 0; x < width; x++) {
 
-			char c = lines[y].at(x);
+			char c = engine[y].at(x);
 			bool isDigit = std::isdigit(c);
 
 			// read last digit
@@ -59,7 +81,7 @@ static void part1() {
 
 							if (ax >= 0 && ax < width && ay >= 0 && ay < height) {
 								
-								char ac = lines[ay].at(ax);
+								char ac = engine[ay].at(ax);
 								if (isSymbol(ac)) {
 									isPartNumber = true;
 								}
@@ -80,11 +102,57 @@ static void part1() {
 }
 
 static void part2() {
-	std::ifstream file("solutions/03/data");
+	int width, height = 0;
+	std::vector<std::string> engine = getEngine(width, height);
 
-	if (!file.is_open()) {
-		throw std::runtime_error("Failed to open file");
+	long total = 0;
+
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+
+			char c = engine[y].at(x);
+			if (c == '*') {
+
+				std::map<XY, int> adjacent;
+
+				for (int ox = -1; ox <= 1; ox++) {
+					for (int oy = -1; oy <= 1; oy++) {
+						if (ox == 0 && oy == 0) continue;
+
+						int ax = x + ox;
+						int ay = y + oy;
+
+						char n = engine[ay].at(ax);
+						if (isdigit(n)) {
+
+							int i0 = ax;
+							while (i0 > 0 && std::isdigit(engine[ay].at(i0 - 1))) {
+								i0--;
+							}
+
+							XY number = XY(i0, ay);
+							if (adjacent.find(number) == adjacent.end()) {
+
+								int i1 = ax;
+								while (i1 < width - 1 && std::isdigit(engine[ay].at(i1 + 1))) {
+									i1++;
+								}
+
+								adjacent[number] = std::stoi(engine[ay].substr(i0, 1 + i1 - i0));
+							}
+						}
+					}
+				}
+
+				if (adjacent.size() == 2) {
+					std::map<XY, int>::iterator iter = adjacent.begin();
+					total += iter++->second * iter->second;
+				}
+			}
+		}
 	}
+
+	std::cout << total << std::endl;
 }
 
 
