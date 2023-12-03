dc18ac3d7c100fdcb400a57d4be679d5878eff1c
Ben Walker <63571454+walkerrobertben@users.noreply.github.com>
2023-12-03 15:27:20 +0000

    Solution 1 part 2

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 COMPILER=clang++
+STD=c++11
 OUTPUT=aoc
 FILES="main.cpp"
 
@@ -7,5 +8,5 @@ for FILE in solutions/*/*.cpp; do
     FILES="$FILES $FILE"
 done
 
-echo "$COMPILER -o $OUTPUT $FILES"
-$COMPILER -o $OUTPUT $FILES
+echo "$COMPILER -std=$STD -o $OUTPUT $FILES"
+$COMPILER -std=$STD -o $OUTPUT $FILES
--- solutions/01/01.cpp
+++ solutions/01/01.cpp
@@ -2,12 +2,37 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
+#include <regex>
+#include <map>
 
-void solution01() {
-	part1();
+static const int CHAR_DIGIT_TO_INT_SHIFT = -48;
+
+static int parseDigit(std::string spelt) {
+	std::map<std::string, int> numberMap = {
+		{"one", 1},
+		{"two", 2},
+		{"three", 3},
+		{"four", 4},
+		{"five", 5},
+		{"six", 6},
+		{"seven", 7},
+		{"eight", 8},
+		{"nine", 9},
+		{"1", 1},
+		{"2", 2},
+		{"3", 3},
+		{"4", 4},
+		{"5", 5},
+		{"6", 6},
+		{"7", 7},
+		{"8", 8},
+		{"9", 9},
+	};
+
+	return numberMap.find(spelt)->second;
 }
 
-void part1() {
+static void part1() {
 	std::ifstream file("solutions/01/data");
 
 	if (!file.is_open()) {
@@ -31,12 +56,46 @@ void part1() {
 			}
 		}
 
-		total += (digit0 - 48) * 10 + (digit1 - 48);
+		total += (digit0 + CHAR_DIGIT_TO_INT_SHIFT) * 10 + (digit1 + CHAR_DIGIT_TO_INT_SHIFT);
 	}
 
 	std::cout << total << std::endl;
 }
 
-void part2() {
+static void part2() {
+	std::ifstream file("solutions/01/data");
 
+	if (!file.is_open()) {
+		throw std::runtime_error("Failed to open file");
+	}
+
+	int total = 0;
+
+	std::regex pattern("(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))");
+
+	std::string line;
+	while (std::getline(file, line)) {
+		std::sregex_iterator next(line.begin(), line.end(), pattern);
+		std::sregex_iterator end;
+		std::string digit0, digit1;
+
+		while (next != end) {
+			std::smatch match = *next;
+			std::string digit = match.str(1);
+			if (digit0.empty()) {
+				digit0 = digit;
+			}
+			digit1 = digit;
+			next++;
+		}
+
+		total += parseDigit(digit0) * 10 + parseDigit(digit1);
+	}
+
+	std::cout << total << std::endl;
+}
+
+void solution01() {
+	part1();
+	part2();
 }
