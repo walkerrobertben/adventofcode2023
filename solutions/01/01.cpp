75303243b8db49422d2c9b272b4f764a0ac323e4
Ben Walker <63571454+walkerrobertben@users.noreply.github.com>
2023-12-03 14:23:57 +0000

    Solution 1 part 1

--- solutions/01/01.cpp
+++ solutions/01/01.cpp
@@ -1,5 +1,42 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
 void solution01() {
-	std::cout << "Hello world" << std::endl;
+	part1();
+}
+
+void part1() {
+	std::ifstream file("solutions/01/data");
+
+	if (!file.is_open()) {
+		throw std::runtime_error("Failed to open file");
+	}
+
+	int total = 0;
+
+	std::string line;
+	while (std::getline(file, line)) {
+		char digit0 = '\0';
+		char digit1;
+
+		for (int i = 0; i < line.size(); i++) {
+			char c = line.at(i);
+			if (std::isdigit(c)) {
+				if (digit0 == '\0') {
+					digit0 = c;
+				}
+				digit1 = c;
+			}
+		}
+
+		total += (digit0 - 48) * 10 + (digit1 - 48);
+	}
+
+	std::cout << total << std::endl;
+}
+
+void part2() {
+
 }
